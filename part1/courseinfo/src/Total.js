8305b70659cc537b8efbcee3b4e952070e98314d
valentinciprian009 <75420902+valentinciprian009@users.noreply.github.com>
2022-08-07 13:46:41 +0300

    Delete part1/courseinfo directory

--- part1/courseinfo/src/Total.js
+++
@@ -1,9 +0,0 @@
-const Total = (props) => {
-    return (
-        <p>Number of exercises {
-            props.parts.map(part => part.exercises).reduce((a, b) => a + b)
-        }</p>
-    )
-}
-
-export default Total

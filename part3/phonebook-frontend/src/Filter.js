c94298a592829569a92ff31a8b7f638f4636a25e
Valentin-Ciprian Popescu <valentin.ciprian009@gmail.com>
2022-08-12 15:57:23 +0300

    Exercices 3.1 to 3.22

---
+++ part3/phonebook-frontend/src/Filter.js
@@ -0,0 +1,9 @@
+const Filter = ({onChangeFunction}) => {
+    return (
+        <div>
+            filter shown with <input onChange={onChangeFunction} />
+        </div>
+    )
+}
+
+export default Filter
